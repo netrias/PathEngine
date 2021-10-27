a79f5158c18b77e59ca207bf6bc0e667e893fafe
Yi-Pei Chen <52116103+bbchen33@users.noreply.github.com>
2021-10-27 13:24:33 -0700

    added new strain name mapping data and AMR data

--- src/preprocessing/reference/fof_strain_mapping.py
+++ src/preprocessing/reference/fof_strain_mapping.py
@@ -1,8 +1,6 @@
-from sys import platform 
 import pandas as pd
-
 def get_mapping_df():
-    ref_path = '../data/strain_name_mapping.xlsx'
+    ref_path = '../data_files/strain_name_mapping.xlsx'
     mapping_df = pd.read_excel(ref_path)
     mapping_df['Name'] = mapping_df['Name'].apply(str).apply(str.lower)
     return mapping_df
